de92a1fa3b5667b27057cba603aca9d636bd4603
shlevanto <simo.levanto@helsinki.fi>
2021-11-11 21:37:55 +0200

    Viikko 3, tehtävä 1 tehty

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,16 @@
 class Player:
-    def __init__(self, name):
-        self.name = name
-    
+    def __init__(self, player_dict):
+        self.name = player_dict['name']
+        self.nationality = player_dict['nationality']
+        self.assists = player_dict['assists']
+        self.goals = player_dict['goals']
+        self.penalties = player_dict['penalties']
+        self.team = player_dict['team']
+        self.games = player_dict['games']
+
     def __str__(self):
-        return self.name
+        return (f"""
+                {self.name} 
+                team {self.team} 
+                goals {self.goals} 
+                assists {self.assists}""")
