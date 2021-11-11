9ca29d90d61f25ad1ea17fb0cc6c3c88be9993ff
shlevanto <simo.levanto@helsinki.fi>
2021-11-11 21:57:11 +0200

    PlayerReader toimii

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,24 +1,11 @@
 import requests
-from player import Player
+from player import PlayerReader
 
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
-    response = requests.get(url).json()
-
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(
-            player_dict
-        )
-
-        players.append(player)
-        
-    players.sort(key=lambda x: x.goals + x.assists, reverse=True)
+    players = PlayerReader(url)    
+    #players.sort(key=lambda x: x.goals + x.assists, reverse=True)
 
     print("Oliot:")
 
