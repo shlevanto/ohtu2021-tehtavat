c06a14a44a36af406478269ac9aa7311278c5387
shlevanto <simo.levanto@helsinki.fi>
2021-11-13 19:11:35 +0200

    validateen lisätty tehtäässä 6 tarvittavat jutut

--- viikko3/login-robot/src/services/user_service.py
+++ viikko3/login-robot/src/services/user_service.py
@@ -1,3 +1,4 @@
+import re
 from entities.user import User
 
 
@@ -38,3 +39,18 @@ class UserService:
             raise UserInputError("Username and password are required")
 
         # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
+        if len(re.findall(r'[^a-z]', username)) > 0:
+            raise UserInputError("Username can only contain characters a-z.")
+
+        if len(re.findall(r'[^a-z]', password)) == 0:
+            raise UserInputError("Password can not be only letters.")
+
+        if len(username) < 3:
+            raise UserInputError("Username too short.")
+
+        if len(password) < 8:
+            raise UserInputError("Password too short.")
+
+        if self._user_repository.find_by_username(username) is not None:
+            raise UserInputError("This username is taken.")
+        
