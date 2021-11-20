d59ae979c8ba276a7208991e3136430117f8505d
shlevanto <simo.levanto@helsinki.fi>
2021-11-20 19:24:02 +0200

    Siirretty testituotteen luominen SetUpiin, lisätty testi Kahden saman tuotteen lisäämise jälkeen ostoskorin hinta on kaksi kertaa tuotteen hinta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -6,26 +6,30 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
-
+        self.bisse = Tuote('Bisse', 3)
+        
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_on_yksi_tavara(self):
-        bisse = Tuote('Bisse', 3)
-        self.kori.lisaa_tuote(bisse)
+        self.kori.lisaa_tuote(self.bisse)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korin_hinta_on_tuotteen_hinta(self):
-        bisse = Tuote('Bisse', 3)
-        self.kori.lisaa_tuote(bisse)
+        self.kori.lisaa_tuote(self.bisse)
 
         self.assertEqual(self.kori.hinta(), 3)
     
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_on_kaksi_tavaraa(self):
-        bisse = Tuote('Bisse', 3)
-        self.kori.lisaa_tuote(bisse)
-        self.kori.lisaa_tuote(bisse)
+        self.kori.lisaa_tuote(self.bisse)
+        self.kori.lisaa_tuote(self.bisse)
         
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+        
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hintaon_kahden_tuotteen_hinta(self):
+        self.kori.lisaa_tuote(self.bisse)
+        self.kori.lisaa_tuote(self.bisse)
+        self.assertEqual(self.kori.hinta(), 6)
+        
