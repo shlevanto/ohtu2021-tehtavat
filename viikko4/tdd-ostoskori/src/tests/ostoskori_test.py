acb1a00c556d10de52fffe1b1b8aaf0c465bfa7d
shlevanto <simo.levanto@helsinki.fi>
2021-11-20 22:52:17 +0200

    Lisätty testikeissi: 15. Metodi tyhjenna tyhjentää korin

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -92,3 +92,13 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
         self.assertEqual(len(self.kori.ostokset()), 0)
+        
+    def test_tyhjenna_tyhjentaa_ostoskorin(self):
+        self.kori.lisaa_tuote(bisse)
+        self.kori.lisaa_tuote(sipsit)
+        self.kori.tyhjenna()
+        
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
+        self.assertEqual(len(self.kori.ostokset()), 0)
+        
