31319f4f4148410fbb35c932cf8e7edc407ea9d3
shlevanto <simo.levanto@helsinki.fi>
2021-11-20 17:18:21 +0200

    2. Lisätty testi, jossa koriin tallennetaan yksi tavara ja tavaroiden määrä palauttaa 1.

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -2,6 +2,7 @@ import unittest
 from ostoskori import Ostoskori
 from tuote import Tuote
 
+
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
@@ -9,3 +10,9 @@ class TestOstoskori(unittest.TestCase):
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_on_yksi_tavara(self):
+        bisse = Tuote('Bisse', 3)
+        self.kori.lisaa_tuote(bisse)
+        
+        self.assertEqual(self.kori.tavaroita_korissa(), 1)
