ed223d2fb1bc19c83da3cee653546ea23da98e9c
Levanto Simo <simo.levanto@ilmarinen.fi>
2021-11-26 14:25:19 +0200

    Pilkottu metodeihin, jotka käsittelevät vähintään neljän pisteen erot (deuce), tasapisteet (even) ja yksinkertaisen pistelaskun (simple). Luovuttu kokonaan score -muuttujasta.

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -12,34 +12,39 @@ class TennisGame:
         else:
             self.m_score2 = self.m_score2 + 1
     
-   
+    def deuce(self):
+        score_difference = self.m_score1 - self. m_score2
+        if score_difference > 2:
+            return "Win for player1"
+        if score_difference < -2:
+            return "Win for player2"
+        
+        differences = {
+            -2: "Win for player2", 
+            -1: "Advantage player2",
+            0: "Deuce",
+            1: "Advantage player1",
+            2: "Win for player1"
+            }
+        
+        return differences[score_difference]
+
+    
+    def even(self):
+        if self.m_score1 < 4:
+            return f"{self.scores[self.m_score1]}-All"
+        
+        return self.deuce()
+    
+    def simple(self):
+        return f"{self.scores[self.m_score1]}-{self.scores[self.m_score2]}"
+
     def get_score(self):
-        score = ""
-        temp_score = 0
+        if self.m_score1 >= 4 or self.m_score2 >= 4:
+            return self.deuce()
 
         if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+            return self.even()
+      
         else:
-            score = f"{self.scores[self.m_score1]}-{self.scores[self.m_score2]}" 
-            
-        return score
+            return self.simple()
