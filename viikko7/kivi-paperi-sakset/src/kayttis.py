1aa7f87785ed8f39a8792daa8fdb8c413fc42455
shlevanto <simo.levanto@helsinki.fi>
2021-12-11 09:44:06 +0200

    Viikko 7, tehtävä 3-4, peliolio luodaan funktion avulla.

--- viikko7/kivi-paperi-sakset/src/kayttis.py
+++ viikko7/kivi-paperi-sakset/src/kayttis.py
@@ -1,14 +1,26 @@
-import kps
+from enum import Enum
+from kps import KPSPelaajaVsPelaaja, KPSTekoaly, KPSParempiTekoaly
 
 
 class Kayttis:
+    def __init__(self):
+        self._komennot = {
+            "a": KPSPelaajaVsPelaaja(),
+            "b": KPSTekoaly(),
+            "c": KPSParempiTekoaly()
+        }
+
+    def _suorita_komento(self, komento):
+        komento.pelaa()
+
     def kaynnista(self):
         while True:
-            print("Valitse pelataanko"
-                "\n (a) Ihmistä vastaan"
-                "\n (b) Tekoälyä vastaan"
-                "\n (c) Parannettua tekoälyä vastaan"
-                "\nMuilla valinnoilla lopetetaan"
+            print(
+                    "Valitse pelataanko"
+                    "\n (a) Ihmistä vastaan"
+                    "\n (b) Tekoälyä vastaan"
+                    "\n (c) Parannettua tekoälyä vastaan"
+                    "\nMuilla valinnoilla lopetetaan"
                 )
 
             vastaus = input()
@@ -18,17 +30,8 @@ class Kayttis:
                     "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
                 )
 
-            if vastaus.endswith("a"):
-                kaksinpeli = kps.KPSPelaajaVsPelaaja()
-                kaksinpeli.pelaa()
-
-            elif vastaus.endswith("b"):
-                yksinpeli = kps.KPSTekoaly()
-                yksinpeli.pelaa()
-
-            elif vastaus.endswith("c"):
-                haastava_yksinpeli = kps.KPSParempiTekoaly()
-                haastava_yksinpeli.pelaa()
+                self._suorita_komento(self._komennot[vastaus])
 
             else:
                 break
+
